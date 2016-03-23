e24eedcf98fbbbbedec6e801ea2d1464c06fbc4e
Daniel Seaton <daniel.seaton87@gmail.com>
2016-03-23 16:58:34 +0000

    Condensed list addition
    The INSERT commands to the list_info and gene_lists tables are now
    processed in one block before committing to the database. This should
    maintain consistency between these tables, since they will either both
    be updated, or neither.

--- load_gene_list_descriptions.py
+++ load_gene_list_descriptions.py
@@ -37,33 +37,24 @@ pub_sql = """INSERT INTO publications (PMID,title,first_author,last_author,journ
         VALUES (%s,%s,%s,%s,%s,%s)"""
 execute_sql_query(pub_sql,pub_info)
 
-
+#check whether list has already been added
 cursor.execute("""SELECT count(*) FROM list_info WHERE list_name=%s AND description=%s AND PMID=%s""",list_info)
 result_count = cursor.fetchall()[0][0]
 if result_count == 0:
-    list_info_sql = """INSERT INTO list_info (list_name,description,PMID) VALUES (%s,%s,%s)"""
-    execute_sql_query(list_info_sql,list_info)
+    try:
+        #insert list into list info
+        cursor.execute("""INSERT INTO list_info (list_name,description,PMID) VALUES (%s,%s,%s)""",list_info)
+        #check whether list_id is already present in database
+        cursor.execute("""SELECT list_id FROM list_info WHERE list_name=%s AND description=%s AND PMID=%s""",list_info)
+        result = cursor.fetchall()[0]
+        #there should only be one list_id with exactly this list info (or the list has been added multiple times)
+        assert len(result)==1
+        list_id = result[0]
+        for locus_id in gene_list:
+            cursor.execute("""INSERT INTO gene_lists (locus_id,list_id) VALUES (%s,%s)""",[locus_id,list_id])
+        db.commit()
+    except:
+        print 'Failed to add list to DB: {0}'.format(*list_info)
+        db.rollback()
 else:
     print "{0} already contained in database".format(list_info[0])
-
-#get list id
-cursor.execute("""SELECT list_id FROM list_info WHERE list_name=%s AND description=%s AND PMID=%s""",list_info)
-result = cursor.fetchall()[0]
-assert len(result)==1
-list_id = result[0]
-
-#check whether list_id is already present in database
-cursor.execute("""SELECT count(*) FROM gene_lists WHERE list_id=%s""",[list_id])
-result_count = cursor.fetchall()[0][0]
-if result_count > 0:
-    #delete previous results
-    cursor.execute("""DELETE FROM gene_lists WHERE list_id=%s""",[list_id])
-
-
-try:
-    #add gene list
-    for locus_id in gene_list:
-        cursor.execute("""INSERT INTO gene_lists (locus_id,list_id) VALUES (%s,%s)""",[locus_id,list_id])
-    db.commit()
-except:
-    db.rollback()
