5b4f725f2d6b32a91dfef4aa1ccbc6230506ca6f
Daniel Seaton <daniel.seaton87@gmail.com>
2016-03-31 09:39:50 +0100

    Deleted connect_to_db.py
    This is now handled by a separate module of the same name,
    since functionality was shared across several locations.

--- connect_to_db.py
+++
@@ -1,8 +0,0 @@
-import MySQLdb
-import os
-
-def connect_to_db():
-    with open(os.getenv('HOME')+'/Dropbox/config/mysqlrootpassword.txt') as f:
-        mysqlpassword = f.readlines()[0].strip()
-    db = MySQLdb.connect("localhost","root",mysqlpassword,"GeneListDB")
-    return db
