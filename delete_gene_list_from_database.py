9310b93d3adbce5ccfe971fb22a13b26a4d26fc9
Daniel Seaton <daniel.seaton87@gmail.com>
2017-03-09 08:44:07 +0000

    Delete gene lists from database if required

--- delete_gene_list_from_database.py
+++ delete_gene_list_from_database.py
@@ -2,10 +2,15 @@ import pandas as pd
 import re
 import sqlite3
 
-#list_name = 'legonaoli2009_toc1_down'
-#list_name = 'legonaoli2009_toc1_up'
+#list_name = 'legnaoli2009_toc1_down'
+#list_name = 'legnaoli2009_toc1_up'
+list_name = 'zhang2009_otsBox_down'
+#list_name = 'zhang2009_otsBox_up'
 
 db = sqlite3.connect('GeneListDB.db')
 cursor = db.cursor()
 
 cursor.execute("DELETE FROM gene_lists WHERE list_name=?", (list_name,))
+cursor.execute("DELETE FROM list_info WHERE list_name=?", (list_name,))
+
+db.close()
